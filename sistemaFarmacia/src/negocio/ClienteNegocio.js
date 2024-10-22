1f3c8ba543ff4ad7041203f27d9c34f3058e9f5e
kimserrano <alejandra.ser.ur@gmail.com>
2024-10-22 03:31:52 -0600

    Se crearon y probaron los métodos para buscar un cliente

--- sistemaFarmacia/src/negocio/ClienteNegocio.js
+++ sistemaFarmacia/src/negocio/ClienteNegocio.js
@@ -29,6 +29,37 @@ class ClienteNegocio {
             throw error;  // Propagamos el error para que lo maneje quien invoque esta función
         }
     }
+
+
+      // Método para buscar un cliente por teléfono
+      static async buscarClientePorTelefono(telefono) {
+        try {
+            if (!telefono || typeof telefono !== 'string') {
+                throw new Error('El teléfono del cliente es obligatorio.');
+            }
+
+            const cliente = await clienteDAO.buscarPorTelefono(telefono);
+            return cliente;
+        } catch (error) {
+            console.error('Error en la capa de negocio al buscar el cliente por teléfono:', error);
+            throw error;  // Propagamos el error para que lo maneje quien invoque esta función
+        }
+    }
+
+    // Método para buscar un cliente por nombre
+    static async buscarClientePorNombre(nombre) {
+        try {
+            if (!nombre || typeof nombre !== 'string') {
+                throw new Error('El nombre del cliente es obligatorio.');
+            }
+
+            const clientes = await clienteDAO.buscarPorNombre(nombre);
+            return clientes;
+        } catch (error) {
+            console.error('Error en la capa de negocio al buscar el cliente por nombre:', error);
+            throw error;  // Propagamos el error para que lo maneje quien invoque esta función
+        }
+    }
 }
 
 module.exports = ClienteNegocio;
