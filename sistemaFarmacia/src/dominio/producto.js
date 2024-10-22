8cd9f02669d532195c7e82552c894b77a413a07e
Diego Robles Rojas <125101098+Castle373@users.noreply.github.com>
2024-10-22 03:30:17 -0700

    Update producto.js

--- sistemaFarmacia/src/dominio/producto.js
+++ sistemaFarmacia/src/dominio/producto.js
@@ -6,6 +6,14 @@ class Producto {
         this.fechaVencimiento = fechaVencimiento;
         this.precio = precio
     }
+     constructor(id, nombre, lote, fechaVencimiento, cantidad, precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.lote = lote;
+        this.fechaVencimiento = fechaVencimiento;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
   }
   
   module.exports = Producto;
